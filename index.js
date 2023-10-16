9f5896c050ca33224f5aa52bb4e0f8d92c8710f9
Eduardo-Ribeiro <eduardoribeiroxavier120@gmail.com>
2023-10-16 15:30:20 -0300

    new Design

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 var botao = document.querySelector("#button");
 
 function getvalue(){
-  var n1 = parseFloat(document.querySelector("#num1").value);
-  var n2 = parseFloat(document.querySelector("#num2").value);
+  var n1 = parseFloat(document.querySelector("#consumo").value);
+  var n2 = parseFloat(document.querySelector("#valor").value);
 
   var soma = (n1+ n2);
  
-  document.getElementById("result").textContent = `A soma é: ${soma}`;
+  document.getElementById("result").textContent = `A soma é: ${soma.toFixed(2)}`;
 }
 
 botao.addEventListener("click", getvalue);
